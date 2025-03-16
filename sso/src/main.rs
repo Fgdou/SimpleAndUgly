828082eb61a100aaa6425fa675ad04ecfdae93b9
Fgdou <fabigoardou@gmail.com>
2025-03-16 11:41:19 +0000

    implement middleware

--- sso/src/main.rs
+++ sso/src/main.rs
@@ -3,21 +3,47 @@ mod objects;
 mod repositories;
 mod services;
 
+use crate::errors::login::{AuthError, LoginError};
+use crate::objects::user::User;
 use crate::repositories::tokens::TokenRepo;
 use crate::repositories::users::UserRepo;
 use crate::services::auth::Auth;
-use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder, Result as AwResult};
-use maud::{html, Markup};
+use actix_web::body::MessageBody;
+use actix_web::cookie::time::OffsetDateTime;
+use actix_web::cookie::Cookie;
+use actix_web::dev::{Service, ServiceRequest, ServiceResponse};
+use actix_web::middleware::{from_fn, Next};
+use actix_web::{get, post, web, App, Error, HttpRequest, HttpResponse, HttpServer, Responder};
+use maud::{html, Markup, PreEscaped};
 use rusqlite::{Connection, OpenFlags};
 use serde::Deserialize;
+use std::rc::Rc;
 use std::sync::{Arc, Mutex};
-use crate::errors::login::LoginError;
+
+async fn login_middleware(
+    req: ServiceRequest,
+    next: Next<impl MessageBody>,
+) -> Result<ServiceResponse<impl MessageBody>, Error> {
+    if req.path() != "/login" {
+        let state: &web::Data<AppState> = req.app_data().unwrap();
+
+        let cookie = match req.cookie("token") {
+            None => return Err(Error::from(AuthError::TokenNotExist)),
+            Some(cookie) => cookie
+        };
+        let user = state.auth.authenticate(cookie.value())?;
+        *state.user.lock().unwrap() = Some(user);
+    }
+    let next = Rc::new(next);
+    Ok(next.call(req).await?)
+}
 
 #[derive(Clone)]
-struct Services {
-    auth: Arc<Auth>
+struct AppState {
+    auth: Arc<Auth>,
+    user: Arc<Mutex<Option<User>>>,
 }
-impl Services {
+impl AppState {
     pub fn new(path: &str) -> Self {
         let connection = Arc::new(Mutex::new(
             Connection::open_with_flags(
@@ -33,8 +59,16 @@ impl Services {
             token_repo: tokens,
         });
 
+        if let None = auth.user_repo.get_user_by_email("admin@example.com") {
+            println!("Info: Creating Admin user");
+            auth.user_repo.add_user(
+                User::new("admin@example.com".to_string(), "Admin".to_string(), "admin".to_string())
+            );
+        }
+
         Self {
             auth,
+            user: Arc::new(Mutex::new(None)),
         }
 
     }
@@ -46,8 +80,69 @@ async fn echo() -> impl Responder {
 }
 
 #[get("/login")]
-async fn login() -> AwResult<Markup> {
-    Ok(html!(
+async fn login() -> impl Responder {
+    login_content(None)
+}
+
+#[derive(Deserialize)]
+struct LoginRequest {
+    email: String,
+    password: String,
+}
+#[post("/login")]
+async fn login_post(body: web::Form<LoginRequest>, state: web::Data<AppState>) -> impl Responder {
+    let token = state.auth.login(&body.email, &body.password);
+
+    let mut response = HttpResponse::Ok();
+
+    match token {
+        Err(e) => response.content_type("text/html")
+            .body(login_content(Some(e))),
+        Ok(token) => {
+
+            let content = html! {
+                (PreEscaped("<script>setTimeout(() => window.location.replace(\"/\"), 3000)</script>"))
+                "You are now connected !"
+            };
+
+            response.cookie(
+                Cookie::build("token", token.value)
+                    .domain("localhost")
+                    .path("/")
+                    .expires(Some(
+                        OffsetDateTime::from_unix_timestamp(token.expiration.unwrap().timestamp())
+                            .unwrap()
+                    ))
+                    .finish()
+            )
+                .body(content)
+        }
+    }
+}
+#[get("/logout")]
+async fn logout(req: HttpRequest, state: web::Data<AppState>) -> impl Responder {
+    state.auth.token_repo.invalidate_token(req.cookie("token").unwrap().value()).unwrap();
+    HttpResponse::Ok()
+        .cookie(
+            Cookie::build("token", "")
+                .expires(OffsetDateTime::now_utc())
+                .finish()
+        )
+        .body(html! {
+            (PreEscaped("<script>setTimeout(() => window.location.replace(\"/\"), 5000)</script>"))
+            "You are now logged out"
+        })
+}
+
+fn login_content(error: Option<LoginError>) -> Markup {
+    html!(
+        @if let Some(error) = error {
+            "Error : " @match error {
+                LoginError::InvalidEmail => "Invalid Email",
+                LoginError::InvalidPassword => "Invalid Password"
+            };
+            br;
+        }
         form action="/login" method="post" {
             input type="text" name="email" placeholder="email";
             br;
@@ -57,30 +152,20 @@ async fn login() -> AwResult<Markup> {
                 "Login"
             };
         }
-    ))
-}
-
-#[derive(Deserialize)]
-struct LoginRequest {
-    email: String,
-    password: String,
-}
-#[post("/login")]
-async fn login_post(body: web::Form<LoginRequest>, auth: web::Data<Auth>) -> impl Responder {
-    Result::<_, LoginError>::Ok(
-        web::Json(auth.login(&body.email, &body.password)?)
     )
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
-        let services = Services::new("/tmp/db.sqlite");
+        let services = AppState::new("/tmp/db.sqlite");
         App::new()
             .service(echo)
             .service(login)
             .service(login_post)
+            .service(logout)
             .app_data(web::Data::new(services))
+            .wrap(from_fn(login_middleware))
     })
         .bind(("0.0.0.0", 8080))?
         .run()
